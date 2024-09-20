549a5eb8323b4d52fd63646af72a4ce9be65a023
Annexode <annexode@gmail.com>
2024-09-20 18:25:12 +0200

    Clipboard landing

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        jamjuree: ["Bai Jamjuree"],
+      },
+      colors: {
+        lightGreen: ["#26bba5"],
+        lightBlue:["#6174ff"]
+      },
+    },
+  },
+  plugins: [],
+};
